978dceadd21efe22257ade700d99a02b7562b26c
damian <koei123@wp.pl>
2019-12-11 20:47:18 +0100

    create database and tutorial with views and urls, add templates folder with html things

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    #polls/5
+    path('<int:question_id>/',views.detail, name = 'detail'),
+    #polls/5/result
+    path('<int:question_id>/result/',views.result, name = 'result'),
+    #polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name = 'vote'),
 ]
